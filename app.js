d9b12d4f428748175dfc85b0a2eea5415da2ca3c
Angelica Garcia <a329528@uach.mx>
2021-05-26 00:11:01 -0600

    Se crea app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer((request, response) => {
+	const file = request.url == '/' ? './319094.json' : `./WWW${request.url}`;
+	
+	fs.readFile(file, (err, data) => {
+		if (err){
+			response.writeHead(400,{"Content-Type":"text/plain"});
+			response.write("Not Found");
+			response.end();
+		}else{
+			const extension = file.split(".").pop();
+				switch (extension) {
+					case 'js':
+						response.writeHead(200, { "Content-Type": "text/javascript" });
+						break;
+					case 'html':
+						response.writeHead(200, { "Content-Type": "text/html" });
+						break;
+					case 'css':
+						response.writeHead(200, { "Content-Type": "text/css" });
+						break;
+					case 'jpeg':
+						response.writeHead(200, { "Content-Type": "image/jpeg" });
+						break;
+					case 'txt':
+						response.writeHead(200, { "Content-Type" : "text/plain" });
+						break;
+					case 'json':
+						response.writeHead(200, { "Content-Type": "application/json" });
+						break;	
+						
+					default:
+						break;
+					}
+				response.write(data);
+				response.end();
+			}
+		});
+}).listen(8888);
+
